f1006d88ef10e7ec32402e94ab88053a7cecd3e8
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-15 12:38:30 -0300

    ✨ Init Resource Form

--- src/components/ResourceForm/index.tsx
+++ src/components/ResourceForm/index.tsx
@@ -1,7 +1,14 @@
-import { Group } from '@mantine/core'
+import { Group, Select, Stack, Text, TextInput } from '@mantine/core'
 import { Back } from 'components/Shared/Back'
+import { Buttons } from 'components/Shared/Buttons'
 import { Title } from 'components/Shared/Title'
 import { useSidebar } from 'contexts/sidebarContext'
+import data from 'data/categories.json'
+import { useState } from 'react'
+import { TbChevronDown } from 'react-icons/tb'
+import { myTheme } from 'styles/theme'
+import { getModalSelectDataCategories } from 'utils/modalSelecDataFormatter'
+import styles from './styles.module.scss'
 
 interface ResourceFormProps {
   isCreateResource?: boolean
@@ -9,15 +16,94 @@ interface ResourceFormProps {
 
 export function ResourceForm({ isCreateResource }: ResourceFormProps) {
   const { setCreateResourceOpened, setChangeResourceOpened } = useSidebar()
+  const [categoryId, setCategoryId] = useState<string | null>(null)
+  console.log('🚀 ~ categoryId', categoryId)
+
+  const resourceCategories = getModalSelectDataCategories(data.categories)
+
+  const title = isCreateResource
+    ? 'Cadastro de recurso'
+    : 'Sugerir alteração de recurso'
 
   return (
-    <Group>
-      <Title name="Sugerir alteração de recurso" />
-      <Back
-        setSidebarOpened={
-          isCreateResource ? setCreateResourceOpened : setChangeResourceOpened
-        }
+    <Stack mt="md" px="md" spacing="md">
+      <Group align="start" position="apart" spacing={0}>
+        <Title name={title} />
+        <Back
+          setSidebarOpened={
+            isCreateResource ? setCreateResourceOpened : setChangeResourceOpened
+          }
+        />
+      </Group>
+
+      <Text>Informações do local</Text>
+
+      <TextInput
+        variant="filled"
+        placeholder="Nome do recurso"
+        label="Nome"
+        radius="md"
+        className={styles.input}
+        required
       />
-    </Group>
+
+      <Select
+        label="Categoria"
+        required
+        size="sm"
+        placeholder="Selecione uma categoria"
+        value={categoryId}
+        variant="filled"
+        onChange={setCategoryId}
+        className={styles.select}
+        maxDropdownHeight={300}
+        rightSectionWidth={30}
+        rightSection={
+          <TbChevronDown size={14} color={myTheme.colors!.brand![0]} />
+        }
+        data={resourceCategories}
+      />
+
+      <TextInput
+        variant="filled"
+        placeholder="Endereço do recurso"
+        label="Endereço"
+        radius="md"
+        className={styles.input}
+        required
+      />
+
+      <Text>Contato</Text>
+
+      <TextInput
+        variant="filled"
+        placeholder="(00) 00000-0000"
+        label="Número de telefone/celular"
+        radius="md"
+        className={styles.input}
+      />
+
+      <TextInput
+        variant="filled"
+        placeholder="Site do recurso"
+        label="Site"
+        radius="md"
+        className={styles.input}
+      />
+
+      <Buttons
+        onCancel={() =>
+          isCreateResource
+            ? setCreateResourceOpened(false)
+            : setChangeResourceOpened(false)
+        }
+        onConfirm={() =>
+          isCreateResource
+            ? setCreateResourceOpened(false)
+            : setChangeResourceOpened(false)
+        }
+        onConfirmText="Enviar"
+      />
+    </Stack>
   )
 }
