3af4690a0fe877043f3cbfa8ee6f05f5fea5db43
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-17 15:10:51 -0300

    ✨ add custom favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,44 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+          <link
+            rel="shortcut icon"
+            href="/favicon/favicon.ico"
+            type="image/x-icon"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#282C4D"
+          />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#282c4d" />
+          <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml"
+          />
+
+          <meta name="theme-color" content="#282C4D" />
         </Head>
         <body>
           <Main />
