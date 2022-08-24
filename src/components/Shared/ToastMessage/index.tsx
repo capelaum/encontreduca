4572abb6402f3f1b203ba73cdd633b34fa91f076
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-24 17:38:49 -0300

    ♻️ refactor: Shared

--- src/components/Shared/ToastMessage/index.tsx
+++ src/components/Shared/ToastMessage/index.tsx
@@ -1,33 +1,3 @@
-import { ReactNode } from 'react'
-import { BsExclamationCircleFill } from 'react-icons/bs'
-import { toast } from 'react-toastify'
-import { Message } from './Message'
+import { showToast, showToastError } from './ToastMessage'
 
-export interface ShowToastProps {
-  title: string
-  description: string
-  icon?: ReactNode
-  dark?: boolean
-}
-
-export const showToast = ({
-  title,
-  description,
-  icon,
-  dark
-}: ShowToastProps) => {
-  toast.success(
-    <Message title={title} description={description} icon={icon} />,
-    {
-      icon: false,
-      theme: dark ? 'dark' : 'light'
-    }
-  )
-}
-
-export const showToastError = ({ title, description }: ShowToastProps) => {
-  toast.error(<Message title={title} description={description} />, {
-    icon: <BsExclamationCircleFill size={24} color="#fff" />,
-    theme: 'colored'
-  })
-}
+export { showToast, showToastError }
