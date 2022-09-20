6ad2edabc5d7f88114c0f98e59718e64c15b5ca6
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-09-20 10:00:29 -0300

    📦 update next + next config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,12 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: [
-      'res.cloudinary.com',
-      'lh1.googleusercontent.com',
-      'lh2.googleusercontent.com',
-      'lh3.googleusercontent.com',
-      'lh4.googleusercontent.com',
-      'lh5.googleusercontent.com',
-      'avatars.githubusercontent.com'
+    domains: ['res.cloudinary.com', 'avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com/**'
+      }
     ]
   }
 }
