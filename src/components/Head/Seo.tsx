890a4b50d0e127d55fc6392e0c27e5ac0aac66b0
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-09-21 18:33:57 -0300

    🔍 update SEO tags + image

--- src/components/Head/Seo.tsx
+++ src/components/Head/Seo.tsx
@@ -7,15 +7,15 @@ interface SeoProps {
 export function Seo({ title, description, url }: SeoProps) {
   return (
     <>
+      <meta name="title" content={title} />
       <meta name="description" content={description} />
-      <meta name="theme-color" content="#66d9e8" />
+      <meta name="theme-color" content="#282C4D" />
 
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-
       <meta property="og:site_name" content="Encontreduca" />
-      <meta property="og:type" content="website" />
       <meta property="og:locale" content="pt_BR" />
       <meta property="og:image" itemProp="image" content="images/Capa.png" />
       <meta property="og:image:type" content="image/png" />
@@ -23,6 +23,7 @@ export function Seo({ title, description, url }: SeoProps) {
       <meta name="twitter:url" content={url} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@capellett" />
+      <meta name="twitter:site" content="@capellett" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content="images/Capa.png" />
